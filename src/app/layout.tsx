57f2ee605d6e792e451147a79c6385d39ac1e5ba
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-04-15 09:50:39 +0700

    feat: config font and background color mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Provider } from "@/components/ui/provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -26,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} antialiased`}>
         <Provider>{children}</Provider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import HomeFeature from "@/modules/home/HomeFeature";
+
 export default function Home() {
   return (
-    <>Test</>
+    <div className="min-h-screen bg-mobile-gradient">
+      <HomeFeature />
+    </div>
   );
 }
