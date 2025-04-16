f48f7a6e9abe35ebea4543aa977a4005ecaf5d34
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-04-16 15:52:03 +0700

    feat: update background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomeFeature from "@/modules/home/HomeFeature";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-mobile-gradient md:min-w-[1200px] lg:max-w-full">
+    <div className="min-h-[2170px] md:min-h-[4619px] bg-mobile-gradient md:bg-desktop-gradient md:min-w-[1200px] lg:max-w-full">
       <HomeFeature />
     </div>
   );
