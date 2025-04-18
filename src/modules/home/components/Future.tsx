af13ec1b0554cc3ae220d7889cafe8117e74dc4c
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-04-18 15:18:45 +0700

    feat: update future in desktop

--- src/modules/home/components/Future.tsx
+++ src/modules/home/components/Future.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 const Future = () => {
   return (
-    <div className="mt-[60px] font-inter px-[20px] text-white relative md:max-w-[1200px] md:mx-auto md:mt-[119px]">
-      <div className="text-[28px] font-bold leading-[110%] w-[176px] text-right">
+    <div className="mt-[60px] font-inter px-[20px] text-white relative md:max-w-[1200px] md:mx-auto md:mt-[180px] md:px-[270px]">
+      <div className="text-[28px] font-bold leading-[110%] w-[176px] text-right md:w-[408px] md:text-[65px]">
         Future <br />
         Applications
       </div>
 
-      <div className="bg-[rgba(255,255,255,0.20)] mt-[13px] ml-[78.37px] px-[5.08px] py-1 rounded-[11.292px] w-[112px]  text-[6px] text-center font-medium">
+      <div className="bg-[rgba(255,255,255,0.20)] mt-[13px] ml-[78.37px] px-[5.08px] py-1 rounded-[11.292px] w-[112px]  text-[6px] text-center font-medium md:mt-[32.07px] md:rounded-[26.829px] md:w-[253.203px] md:h-[46.113px] md:text-[13px] md:flex md:items-center md:justify-center md:ml-[169.03px]">
         Prepare for the New Age of Health
       </div>
 
-      <div className="mt-[82px]">
+      <div className="mt-[82px] md:mt-[196px]">
         <Image
           src="/svg/future/iPhone-14.svg"
           alt="iPhone-14"
@@ -21,93 +21,99 @@ const Future = () => {
         />
       </div>
 
-      <div className="w-[68.157px] h-[68.157px] bg-[radial-gradient(50%_50%_at_50%_50%,_#BAEFF8_0%,_#95D5FA_100%)] shadow-[3.529px_2.823px_1.412px_0px_#BAE6FF_inset] rounded-[13.7px] px-[11.64px] pt-[9.53px] pb-[9.69px] absolute top-[12px] right-[87px]">
+      <div className="w-[68.157px] h-[68.157px] bg-[radial-gradient(50%_50%_at_50%_50%,_#BAEFF8_0%,_#95D5FA_100%)] shadow-[3.529px_2.823px_1.412px_0px_#BAE6FF_inset] rounded-[13.7px] px-[11.64px] pt-[9.53px] pb-[9.69px] absolute top-[12px] right-[87px] md:w-[161.934px] md:h-[161.934px] md:rounded-[32.549px] md:shadow-[8.384px_6.707px_3.354px_0px_#BAE6FF_inset] md:pl-[25.15px] md:pt-[22.64px] md:right-[321px] md:top-[26px]">
         <div>
           <Image
             src="/svg/future/chuyenkhoa.svg"
             alt="icon-1"
-            width={21.916}
-            height={23.16}
+            width={30.348}
+            height={28.985}
+            className="md:w-[72.104px] md:h-[68.865px]"
           />
         </div>
 
-        <div className="mt-[6.89px] pl-[3.18px] text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B]">
-          Hơn 150 chuyên khoa
+        <div className="mt-[6.89px] pl-[3.18px] text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B] md:mt-[10.06px] md:text-[15px]">
+          Hơn 150 <br className="hidden md:block" /> chuyên khoa
         </div>
       </div>
 
-      <div className="w-[66.102px] h-[66.102px] bg-[url('/svg/future/bg-hoidap.svg')] bg-cover bg-center shadow-[3.529px_2.823px_1.412px_0px_#8CCCF1_inset] rounded-[13.7px] pl-[10px] pt-[19.5px] pb-[9.69px] absolute top-[84px] left-[24px]">
+      <div className="w-[66.102px] h-[66.102px] bg-[url('/svg/future/bg-hoidap.svg')] bg-cover bg-center shadow-[3.529px_2.823px_1.412px_0px_#8CCCF1_inset] rounded-[13.7px] pl-[10px] pt-[19.5px] pb-[9.69px] absolute top-[84px] left-[24px] md:w-[157.051px] md:h-[157.051px] md:rounded-[32.549px] md:shadow-[8.384px_6.707px_3.354px_0px_#8CCCF1_inset] md:pt-[48.59px] md:pl-[30.18px] md:left-[266px] md:top-[203px]">
         <div>
           <Image
             src="/svg/future/hoidap.svg"
             alt="icon-2"
-            width={18.755}
+            width={24.702}
             height={18.851}
+            className="md:w-[58.689px] md:h-[56.053px]"
           />
         </div>
 
-        <div className="mt-[4.21px] pl-[3.18px] text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B]">
+        <div className="mt-[4.21px] pl-[3.18px] text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B] md:mt-[10px] md:text-[15px]">
           Hỏi đáp 24/7
         </div>
       </div>
 
-      <div className="w-[78.089px] h-[78.089px] bg-[url('/svg/future/bg-ai-support.svg')] bg-cover bg-center shadow-[3.529px_2.823px_1.412px_0px_rgba(125,171,203,0.40)_inset] rounded-[13.7px] pl-[10px] pt-[16px] pb-[9.69px] absolute top-[121px] left-[100px]">
+      <div className="w-[78.089px] h-[78.089px] bg-[url('/svg/future/bg-ai-support.svg')] bg-cover bg-center shadow-[3.529px_2.823px_1.412px_0px_rgba(125,171,203,0.40)_inset] rounded-[13.7px] pl-[10px] pt-[16px] pb-[9.69px] absolute top-[121px] left-[100px] md:w-[185.532px] md:h-[185.532px] md:shadow-none md:pt-[43.23px] md:pl-[28.04px] md:top-[284px] md:left-[446px]">
         <div className="pl-2">
           <Image
             src="/svg/future/ai-support.svg"
             alt="icon-3"
             width={19.084}
             height={26.938}
+            className="md:w-[45.314px] md:h-[63.401px]"
           />
         </div>
 
-        <div className="mt-[7.06px] pl-[3.18px] pr-1 text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B]">
+        <div className="mt-[7.06px] pl-[3.18px] pr-1 text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B] md:mt-[12.26px] md:text-[15px]">
           AI hỗ trợ phân tích kết quả khám
         </div>
       </div>
 
-      <div className="w-[61.649px] h-[61.649px] bg-[url('/svg/future/bg-bacsi.svg')] bg-cover bg-center shadow-[3.529px_2.823px_1.412px_0px_rgba(134,205,255,0.30)_inset] rounded-[13.7px] pl-[14.32px] pt-[14.96px] pb-[9.69px] absolute top-[104px] left-[190px]">
+      <div className="w-[61.649px] h-[61.649px] bg-[url('/svg/future/bg-bacsi.svg')] bg-cover bg-center shadow-[3.529px_2.823px_1.412px_0px_rgba(134,205,255,0.30)_inset] rounded-[13.7px] pl-[14.32px] pt-[14.96px] pb-[9.69px] absolute top-[104px] left-[190px] md:w-[146.473px] md:h-[146.473px] md:shadow-none md:pt-[35.55px] md:pl-[31.71px] md:top-[247px] md:left-[659px]">
         <div>
           <Image
             src="/svg/future/bacsi.svg"
             alt="icon-4"
-            width={21.156}
-            height={20.863}
+            width={27.338}
+            height={26.109}
+            className="md:w-[64.951px] md:h-[62.033px]"
           />
         </div>
 
-        <div className="mt-[4.4px] pl-[3.18px] pr-1 text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B]">
+        <div className="mt-[4.4px] pl-[3.18px] pr-1 text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B] md:mt-[10.45px] md:text-[15px]">
           500⁺ Bác sĩ
         </div>
       </div>
 
-      <div className="w-[78.089px] h-[78.089px] bg-[url('/svg/future/bg-giaiphap.svg')] bg-cover bg-center shadow-[3.529px_2.823px_1.412px_0px_rgba(134,205,255,0.30)_inset] rounded-[13.7px] pl-[14.09px] pt-[22.87px] pb-[9.69px] absolute top-[175px] left-[203px]">
+      <div className="w-[78.089px] h-[78.089px] bg-[url('/svg/future/bg-giaiphap.svg')] bg-cover bg-center shadow-[3.529px_2.823px_1.412px_0px_rgba(134,205,255,0.30)_inset] rounded-[13.7px] pl-[14.09px] pt-[22.87px] pb-[9.69px] absolute top-[175px] left-[203px] md:w-[185.532px] md:h-[185.532px] md:rounded-[32.549px] md:pt-[53px] md:pl-[35.93px] md:pr-[24px] md:top-[418px] md:left-[691px]">
         <div className="pl-[3.19px]">
           <Image
             src="/svg/future/giaiphap.svg"
             alt="icon-5"
-            width={26.804}
-            height={20.746}
+            width={33.171}
+            height={30.348}
+            className="md:w-[78.812px] md:h-[72.104px]"
           />
         </div>
 
-        <div className="mt-[2.48px] pl-[3.18px] pr-1 text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B]">
+        <div className="mt-[2.48px] pl-[3.18px] pr-1 text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B] md:mt-[7.06px] md:text-[15px]">
           Giải pháp tối ưu khi tìm thuốc
         </div>
       </div>
 
-      <div className="w-[78.089px] h-[78.089px] bg-[url('/svg/future/bg-luutru.svg')] bg-cover bg-center shadow-[8.384px_6.707px_3.354px_0px_rgba(127, 226, 240, 0.40)_inset] rounded-[13.7px] pl-[13.06px] pt-[17.15px] pb-[9.69px] absolute top-[85px] left-[262px]">
-        <div className="pl-[3.19px]">
+      <div className="w-[78.089px] h-[78.089px] bg-[url('/svg/future/bg-luutru.svg')] bg-cover bg-center shadow-[8.384px_6.707px_3.354px_0px_rgba(127, 226, 240, 0.40)_inset] rounded-[13.7px] pl-[13.06px] pt-[17.15px] pb-[9.69px] absolute top-[85px] left-[262px] md:w-[185.532px] md:h-[185.532px] md:pt-[36px] md:pl-[20.12px] md:pb-[30.82px] md:top-[200px] md:left-[830px]">
+        <div className="pl-[3.19px] md:pl-0">
           <Image
             src="/svg/future/luutru.svg"
             alt="icon-6"
-            width={26.804}
-            height={20.746}
+            width={31.85}
+            height={30.419}
+            className="md:w-[75.672px] md:h-[72.273px]"
           />
         </div>
 
-        <div className="mt-[4.87px] pl-[3.18px] pr-1 text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B]">
-          Lưu trữ & bảo mật hồ sơ khám
+        <div className="mt-[4.87px] pl-[3.18px] pr-1 text-[6.5px] font-semibold leading-[120%] text-[#1D1B1B] md:mt-[10px] md:text-[15px] md:top-[201px] md:left-[829px]">
+          Lưu trữ & bảo <br /> mật hồ sơ khám
         </div>
       </div>
     </div>
