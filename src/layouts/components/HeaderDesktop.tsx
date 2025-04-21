69c518e52dd8c1f19588174394e6c61fcbaebd1d
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-04-21 10:27:16 +0700

    feat: update dropdown menu header

--- src/layouts/components/HeaderDesktop.tsx
+++ src/layouts/components/HeaderDesktop.tsx
@@ -26,41 +26,55 @@ const HeaderDesktop = () => {
             Trang chủ
           </button>
 
-          <Menu.Root onOpenChange={(details) => setIsMenuOpen(details.open)}>
-            <Menu.Trigger asChild>
-              <Button
-                className={`gap-2 items-center justify-center px-6 py-2.5 rounded-[78.638px] text-[15px] font-medium leading-[15.726px] text-white font-roboto ${
-                  isMenuOpen ? "bg-button-hover" : "hover:bg-button-hover"
-                }`}
-              >
-                <span className="font-roboto font-medium leading-[15.728px]">
-                  Danh sách chi tiết
-                </span>
+          <div className="relative">
+            <Menu.Root 
+              open={isMenuOpen}
+              onOpenChange={(details) => setIsMenuOpen(details.open)}
+            >
+              <Menu.Trigger asChild>
+                <Button
+                  onPointerEnter={() => setIsMenuOpen(true)}
+                  className={`gap-2 items-center justify-center px-6 py-2.5 rounded-[78.638px] text-[15px] font-medium leading-[15.726px] text-white font-roboto ${
+                    isMenuOpen ? "bg-button-hover" : "hover:bg-button-hover"
+                  }`}
+                >
+                  <span className="font-roboto font-medium leading-[15.728px]">
+                    Danh sách chi tiết
+                  </span>
 
-                <Image
-                  src="/svg/chevron-down.svg"
-                  alt="arrow-down"
-                  width={18}
-                  height={18}
-                />
-              </Button>
-            </Menu.Trigger>
-            <Portal>
-              <Menu.Positioner className="w-[229px]  ">
-                <Menu.Content className="rounded-[16px] pt-[14px] pb-[14px] px-0 bg-gradient-7 ">
-                  {DETAIL_LIST.map((item) => (
-                    <Menu.Item
-                      key={item.id}
-                      value={item.id}
-                      className="hover:cursor-pointer px-4  text-white font-roboto hover:bg-[rgba(0,0,0,0.10)] hover:font-semibold transition-all duration-200"
-                    >
-                      {item.title}
-                    </Menu.Item>
-                  ))}
-                </Menu.Content>
-              </Menu.Positioner>
-            </Portal>
-          </Menu.Root>
+                  <Image
+                    src="/svg/chevron-down.svg"
+                    alt="arrow-down"
+                    width={18}
+                    height={18}
+                  />
+                </Button>
+              </Menu.Trigger>
+              <Portal>
+                <Menu.Positioner className="w-[229px]">
+                  <div 
+                    className="absolute w-full h-4 -top-4"
+                    onPointerEnter={() => setIsMenuOpen(true)}
+                  />
+                  <Menu.Content 
+                    className="rounded-[16px] pt-[14px] pb-[14px] px-0 bg-gradient-7"
+                    onPointerEnter={() => setIsMenuOpen(true)}
+                    onPointerLeave={() => setIsMenuOpen(false)}
+                  >
+                    {DETAIL_LIST.map((item) => (
+                      <Menu.Item
+                        key={item.id}
+                        value={item.id}
+                        className="hover:cursor-pointer px-4 text-white font-roboto hover:bg-[rgba(0,0,0,0.10)] hover:font-semibold transition-all duration-200"
+                      >
+                        {item.title}
+                      </Menu.Item>
+                    ))}
+                  </Menu.Content>
+                </Menu.Positioner>
+              </Portal>
+            </Menu.Root>
+          </div>
 
           <button className="inline-flex items-center justify-center px-6 py-2.5 rounded-[78.638px] text-[15px] font-medium leading-[15.726px] text-white font-roboto hover:bg-button-hover">
             Đăng ký đối tác
