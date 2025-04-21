6de5bf02385a41127f1dc75ddf2220a4d5b1e197
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-04-21 13:10:35 +0700

    feat: animation introduction

--- src/modules/home/components/Introduction/Introduction.tsx
+++ src/modules/home/components/Introduction/Introduction.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import IntroductionDoctor from "./IntroductionDoctor";
 import IntroductionMedicine from "./IntroductionMedicine";
+import { motion } from "framer-motion";
 
 const Introduction = () => {
   const [isFindDoctor, setIsFindDoctor] = useState(true);
@@ -18,7 +19,13 @@ const Introduction = () => {
 
   return (
     <div className="px-[7px] md:max-w-[1200px] md:mx-auto md:mt-[52px] relative z-10 font-inter">
-      <div className="rounded-[3113.76px] border-[0.331px] text-white px-[10px] py-[8px] flex items-center gap-1 hover:cursor-pointer max-w-[245px] mx-auto md:px-[33.5px] md:py-[10px] md:max-w-max">
+      <motion.div 
+        initial={{ y: 50, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+        className="rounded-[3113.76px] border-[0.331px] text-white px-[10px] py-[8px] flex items-center gap-1 hover:cursor-pointer max-w-[245px] mx-auto md:px-[33.5px] md:py-[10px] md:max-w-max"
+      >
         <div className="text-[9.868px] font-medium leading-[4.917px] md:text-sm md:font-semibold md:leading-4">
           Cập nhật phiên bản mới nhất của{" "}
           <span className="font-semibold">Ứng dụng</span>
@@ -33,28 +40,46 @@ const Introduction = () => {
             className="md:w-3 md:h-3"
           />
         </div>
-      </div>
+      </motion.div>
 
       <div className="mt-4 text-white">
-        <div className="font-roboto text-center text-[30px] font-black leading[120%] text-shadow md:mt-8 md:text-[96px] md:text-shadow-2">
+        <motion.div 
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="font-roboto text-center text-[30px] font-black leading[120%] text-shadow md:mt-8 md:text-[96px] md:text-shadow-2"
+        >
           Kết nối cùng Y Viet
-        </div>
+        </motion.div>
 
-        <div className="mt-3 text-center text-[8px] font-medium leading-[11px] mx-auto md:mt-8 md:text-[20px] md:leading-[28px] md:w-auto">
+        <motion.div 
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+          className="mt-3 text-center text-[8px] font-medium leading-[11px] mx-auto md:mt-8 md:text-[20px] md:leading-[28px] md:w-auto"
+        >
           <div>
             Điểm dừng đầu tiên để bạn kết nối các dịch vụ tư vấn y khoa chất
             lượng cao
           </div>
           <div>
             Đơn giản hóa trải nghiệm chăm sóc sức khỏe của bạn chỉ với{" "}
-            <span className="font-bold">“một lần chạm”</span>
+            <span className="font-bold">&quot;một lần chạm&quot;</span>
           </div>
-        </div>
+        </motion.div>
 
-        <div className="mt-[11px] flex items-center justify-center gap-2 md:mt-8 md:gap-4">
+        <motion.div 
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 0.6 }}
+          className="mt-[11px] flex items-center justify-center gap-2 md:mt-8 md:gap-4"
+        >
           <button
             onClick={handleChangeFindDoctor}
-            className={`hover:cursor-pointer hover:opacity-80 px-4 py-2 rounded-[32px]  text-[10px] font-medium leading-[6.228px] md:rounded-[20px] md:text-[14px] md:leading-[20px] ${
+            className={`hover:cursor-pointer hover:opacity-80 px-4 py-2 rounded-[32px] text-[10px] font-medium leading-[6.228px] md:rounded-[20px] md:text-[14px] md:leading-[20px] ${
               isFindDoctor
                 ? "bg-[rgba(255,255,255,0.38)] shadow-[0px_0.311px_0.623px_-0.311px_rgba(0,0,0,0.10),_0px_0.311px_0.934px_0px_rgba(0,0,0,0.10)]"
                 : "border-[0.089px] shadow-[0px_0.311px_0.623px_-0.311px_rgba(0,0,0,0.10),_0px_0.311px_0.934px_0px_rgba(0,0,0,0.10)]"
@@ -65,7 +90,7 @@ const Introduction = () => {
 
           <button
             onClick={handleChangeFindMedicine}
-            className={`hover:cursor-pointer hover:opacity-80 px-4 py-2 rounded-[32px]  text-[10px] font-medium leading-[6.228px] md:rounded-[20px] md:text-[14px] md:leading-[20px] ${
+            className={`hover:cursor-pointer hover:opacity-80 px-4 py-2 rounded-[32px] text-[10px] font-medium leading-[6.228px] md:rounded-[20px] md:text-[14px] md:leading-[20px] ${
               !isFindDoctor
                 ? "bg-[rgba(255,255,255,0.38)] shadow-[0px_0.311px_0.623px_-0.311px_rgba(0,0,0,0.10),_0px_0.311px_0.934px_0px_rgba(0,0,0,0.10)]"
                 : "border-[0.089px] shadow-[0px_0.311px_0.623px_-0.311px_rgba(0,0,0,0.10),_0px_0.311px_0.934px_0px_rgba(0,0,0,0.10)]"
@@ -73,7 +98,7 @@ const Introduction = () => {
           >
             Tìm thuốc
           </button>
-        </div>
+        </motion.div>
       </div>
 
       {!!isFindDoctor ? <IntroductionDoctor /> : <IntroductionMedicine />}
