a37b6899ecba3f7876bcfd8fa30c04da47025feb
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-04-21 09:57:15 +0700

    feat: add dropdown menu header

--- src/layouts/components/HeaderDesktop.tsx
+++ src/layouts/components/HeaderDesktop.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import Image from "next/image";
+import { Button, Menu, Portal } from "@chakra-ui/react";
+import { DETAIL_LIST } from "@/variables";
+import { useState } from "react";
 
 const HeaderDesktop = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="hidden md:flex items-center justify-between md:px-[28px]">
       <div>
@@ -19,18 +26,41 @@ const HeaderDesktop = () => {
             Trang chủ
           </button>
 
-          <button className="inline-flex gap-2 items-center justify-center px-6 py-2.5 rounded-[78.638px]  text-[15px] font-medium leading-[15.726px] text-white font-roboto hover:bg-button-hover">
-            <span className="font-roboto font-medium leading-[15.728px]">
-              Danh sách chi tiết
-            </span>
+          <Menu.Root onOpenChange={(details) => setIsMenuOpen(details.open)}>
+            <Menu.Trigger asChild>
+              <Button
+                className={`gap-2 items-center justify-center px-6 py-2.5 rounded-[78.638px] text-[15px] font-medium leading-[15.726px] text-white font-roboto ${
+                  isMenuOpen ? "bg-button-hover" : "hover:bg-button-hover"
+                }`}
+              >
+                <span className="font-roboto font-medium leading-[15.728px]">
+                  Danh sách chi tiết
+                </span>
 
-            <Image
-              src="/svg/chevron-down.svg"
-              alt="arrow-down"
-              width={18}
-              height={18}
-            />
-          </button>
+                <Image
+                  src="/svg/chevron-down.svg"
+                  alt="arrow-down"
+                  width={18}
+                  height={18}
+                />
+              </Button>
+            </Menu.Trigger>
+            <Portal>
+              <Menu.Positioner className="w-[229px]  ">
+                <Menu.Content className="rounded-[16px] pt-[14px] pb-[14px] px-0 bg-gradient-7 ">
+                  {DETAIL_LIST.map((item) => (
+                    <Menu.Item
+                      key={item.id}
+                      value={item.id}
+                      className="hover:cursor-pointer px-4  text-white font-roboto hover:bg-[rgba(0,0,0,0.10)] hover:font-semibold transition-all duration-200"
+                    >
+                      {item.title}
+                    </Menu.Item>
+                  ))}
+                </Menu.Content>
+              </Menu.Positioner>
+            </Portal>
+          </Menu.Root>
 
           <button className="inline-flex items-center justify-center px-6 py-2.5 rounded-[78.638px] text-[15px] font-medium leading-[15.726px] text-white font-roboto hover:bg-button-hover">
             Đăng ký đối tác
