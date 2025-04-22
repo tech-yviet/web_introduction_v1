cb742277e209cba670099547374190db7b88c2f4
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-04-22 15:25:23 +0700

    feat: create card doctor in mobile

--- src/modules/doctors/DoctorsFeature.tsx
+++ src/modules/doctors/DoctorsFeature.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useEffect } from "react";
+import { FC, useEffect, useMemo } from "react";
 import { connect, ConnectedProps, dispatch, RootState } from "@/store";
 import { doctorsA } from "@/store/modules/doctors";
 import Footer from "@/layouts/Footer";
@@ -41,56 +41,72 @@ const $DoctorsFeature: FC<PropsFromRedux> = () => {
     dispatch(appA.toggleDrawerMenuMobile());
   };
 
-  const filterDoctors = [
-    {
-      id: "0",
-      name: "Tất cả",
-      value: FILTER_DOCTORS.ALL,
-    },
-    {
-      id: "1",
-      name: "Răng hàm mặt ",
-      value: FILTER_DOCTORS.RANG_HAM_MAT,
-    },
-    {
-      id: "2",
-      name: "Tim mạch",
-      value: FILTER_DOCTORS.TIM_MACH,
-    },
-    {
-      id: "3",
-      name: "Da liễu",
-      value: FILTER_DOCTORS.DA_LIEU,
-    },
-    {
-      id: "4",
-      name: "Hô hấp",
-      value: FILTER_DOCTORS.HO_HAP,
-    },
-    {
-      id: "5",
-      name: "Than tiết niệu",
-      value: FILTER_DOCTORS.THAN_TIET_NIU,
-    },
-    {
-      id: "6",
-      name: "Nội tết đại thao duong",
-      value: FILTER_DOCTORS.NOI_TIET_DAI_THAO_DUONG,
-    },
-    {
-      id: "7",
-      name: "Tieu hóa",
-      value: FILTER_DOCTORS.TIEU_HOA,
-    },
-    {
-      id: "8",
-      name: "Cơ xương khớp",
-      value: FILTER_DOCTORS.CO_XUONG_KHOP,
-    },
-  ];
+  const filterDoctors = useMemo(() => {
+    return [
+      {
+        id: "0",
+        name: "Tất cả",
+        value: FILTER_DOCTORS.ALL,
+      },
+      {
+        id: "1",
+        name: "Răng hàm mặt ",
+        value: FILTER_DOCTORS.RANG_HAM_MAT,
+      },
+      {
+        id: "2",
+        name: "Tim mạch",
+        value: FILTER_DOCTORS.TIM_MACH,
+      },
+      {
+        id: "3",
+        name: "Da liễu",
+        value: FILTER_DOCTORS.DA_LIEU,
+      },
+      {
+        id: "4",
+        name: "Hô hấp",
+        value: FILTER_DOCTORS.HO_HAP,
+      },
+      {
+        id: "5",
+        name: "Than tiết niệu",
+        value: FILTER_DOCTORS.THAN_TIET_NIU,
+      },
+      {
+        id: "6",
+        name: "Nội tết đại thao duong",
+        value: FILTER_DOCTORS.NOI_TIET_DAI_THAO_DUONG,
+      },
+      {
+        id: "7",
+        name: "Tieu hóa",
+        value: FILTER_DOCTORS.TIEU_HOA,
+      },
+      {
+        id: "8",
+        name: "Cơ xương khớp",
+        value: FILTER_DOCTORS.CO_XUONG_KHOP,
+      },
+    ];
+  }, []);
+
+  const doctors = useMemo(() => {
+    return Array.from({ length: 10 }, (_, index) => ({
+      id: `${index}`,
+      doctorFirstName: "BS",
+      doctorLastName: "Trịnh Thụy Thùy ",
+      subSpecialtyNameVi: "Nội tổng quát",
+      subMainType: "INTERNAL_MEDICINE",
+      address: "Bệnh viện Đại học Y Dược TP.HCM",
+      appointmentCount: 18,
+      hasWorkingTime: true,
+      image: "/img/bs-nu.png",
+    }));
+  }, []);
 
   return (
-    <div className="pt-[113px] bg-doctors-gradient-mobile h-screen flex flex-col font-roboto">
+    <div className="pt-[113px] bg-doctors-gradient-mobile  flex flex-col font-roboto">
       <div className="flex-1 px-4">
         <div className="fixed top-[40px] left-0 right-0 bg-white z-50 md:hidden font-inter px-[10px] py-[6px] bg-gradient-6 rounded-b-[16px] flex items-center gap-[15px]">
           <div className="p-3 flex items-center  bg-white rounded-[40px] flex-1 h-[32px]">
@@ -161,7 +177,88 @@ const $DoctorsFeature: FC<PropsFromRedux> = () => {
           ))}
         </div>
 
-        <div>Danh sách bác sĩ</div>
+        <div className="grid grid-cols-1 gap-3">
+          {doctors.map((doctor) => (
+            <div
+              key={doctor.id}
+              className="flex items-center gap-[8.73px] bg-white rounded-[12.4px] border-doctors-card py-[6.35px] pl-[6.27px] box-shadow-doctors-card hover:cursor-pointer"
+            >
+              <div className="bg-[#E6F1FF] rounded-[9.3px] px-[2.5px] pt-[5px]">
+                <Image
+                  src={doctor.image}
+                  alt={`${doctor.doctorFirstName} ${doctor.doctorLastName}`}
+                  width={88}
+                  height={88}
+                />
+              </div>
+
+              <div className="flex-1">
+                <div className="text-[#1F2A37] font-medium mb-2">{`${doctor.doctorFirstName} ${doctor.doctorLastName}`}</div>
+
+                <div className="flex flex-col gap-[4.75px]  font-inter text-xs text-[#8E8E8E]">
+                  <div className="flex items-center gap-[6.2px]">
+                    <div>
+                      <Image
+                        src="/svg/icons/school.svg"
+                        alt="school"
+                        width={15}
+                        height={15}
+                      />
+                    </div>
+
+                    <div className=" ">{doctor.subSpecialtyNameVi}</div>
+                  </div>
+
+                  <div className="flex items-center gap-[6.2px]">
+                    <div>
+                      <Image
+                        src="/svg/icons/location.svg"
+                        alt="location"
+                        width={15}
+                        height={16}
+                      />
+                    </div>
+
+                    <div>{doctor.address}</div>
+                  </div>
+
+                  <div className="flex items-center gap-[17.68px]">
+                    <div className="flex items-center gap-[6.2px]">
+                      <div>
+                        <div>
+                          <Image
+                            src="/svg/icons/star.svg"
+                            alt="star"
+                            width={13}
+                            height={13}
+                          />
+                        </div>
+                      </div>
+
+                      <div>9.5/10</div>
+                    </div>
+
+                    <div className="flex items-center gap-[6.2px]">
+                      <div>
+                        <div>
+                          <Image
+                            src="/svg/icons/calendar.svg"
+                            alt="calendar"
+                            width={11.625}
+                            height={11.625}
+                          />
+                        </div>
+                      </div>
+
+                      <div>{doctor.appointmentCount} lượt đặt</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
         <div>Pagination</div>
       </div>
 
