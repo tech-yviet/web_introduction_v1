14156e31a7d8c0b8a6e0eb3e6d6819c4e8edfee9
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-05-22 14:13:24 +0700

    fix: Font size, font weight của các content ở phần Tìm Bác sĩ, Tìm thuốc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Provider } from "@/components/ui/provider";
 import { ReduxProvider } from "@/core/providers/ReduxProvider";
 
 const inter = Inter({
-  subsets: ["latin"],
+  subsets: ["latin", "vietnamese"],
   variable: "--font-inter",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   display: "swap",
